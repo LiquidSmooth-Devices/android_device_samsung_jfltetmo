7299a1c2deb3fb4971fe964bdc1ccacbcda1f104
CPA-Poke <brock.shahan@cox.net>
2015-01-23 23:28:23 -0500

    Hello jfltetmo!
    Change-Id: I602707b0639811bfa378b81d1e8b21219af14a9e

--- liquid.mk
+++ liquid.mk
@@ -1,10 +1,10 @@
 $(call inherit-product, device/samsung/jfltetmo/full_jfltetmo.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/liquid/config/nfc_enhanced.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common Liquid stuff.
+$(call inherit-product, vendor/liquid/config/common_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
   PRODUCT_NAME=jfltetmo \
@@ -12,6 +12,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
   BUILD_FINGERPRINT="samsung/jfltetmo/jfltetmo:4.4.4/KTU84P/M919UVUFNK2:user/release-keys" \
   PRIVATE_BUILD_DESC="jfltetmo-user 4.4.4 KTU84P M919UVUFNK2 release-keys"
 
-PRODUCT_NAME := cm_jfltetmo
+PRODUCT_NAME := liquid_jfltetmo
 PRODUCT_DEVICE := jfltetmo
 
